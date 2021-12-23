a28d79de9b1f9975eeb93902e79e07afb0831095
Tojimurodovsardor25 <tojimuradovsardor25@gmail.com>
2021-12-23 11:37:51 +0500

    Create-post-start : )

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const { engine } = require('express-handlebars')
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const app = express();
 const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const contentRouter = require('./routes/content')
 
+// const variables = require('./')
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,12 +24,17 @@ app.engine('hbs', engine({
   defaultLayout: 'layout',
   extname: 'hbs',
   partialsDir: [path.join(__dirname, 'views/partials')],
+
   runtimeOptions: {
     allowProtoMethodsByDefault: true,
     allowProtoMethodsByDefault: true
   }
 }))
 
+const store = new MongoStore({
+  uri: 'mongodb+srv://mirzaabdullayev:GyeI0l6BlW34k7aR@adminpanel.3hbpu.mongodb.net/adminPanel',
+  collection: 'session'
+})
 
 require('./helper/db')()
 
@@ -36,14 +44,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  resave: false,
+  secret: 'some_secret_key',
+  saveUninitialized: false,
+  store
+}))
+
+// app.use
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin/contentCreate', contentRouter)
+app.use('/admin/content', contentRouter)
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
--- routes/content.js
+++ routes/content.js
@@ -1,13 +1,26 @@
 const { Router } = require('express')
-// const Content = require('../models/Content')
+const Content = require('../models/Content')
 const router = Router()
 
 
-router.get('/view', (req, res) => {
-    res.render('admin/contents', {
+router.get('/add', (req, res) => {
+    res.render('admin/contentCreate', {
         title: 'Admin bo`limi',
         layout: 'main',
     })
+})
+
+router.post('/add', async (req, res) => {
+    const { contentName, contentContext, img } = req.body
+    console.log(req.body);
+    const content = new Content({
+        contentName,
+        contentContext,
+        img
+    })
+
+    await content.save()
+    res.redirect('/')
 })
 
 module.exports = router
