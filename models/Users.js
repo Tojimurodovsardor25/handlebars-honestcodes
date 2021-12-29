cd02e5455026727c9feb57026eb4abf588b6b8dd
Tojimurodovsardor25 <tojimuradovsardor25@gmail.com>
2021-12-29 13:48:30 +0500

    29.12 davomi bor : )

---
+++ models/Users.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose')
+
+
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    nickname: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    contentId: {
+        ref: 'contents',
+        type: Schema.Types.ObjectId
+    }
+})
+
+module.exports = model('user', UserSchema)
