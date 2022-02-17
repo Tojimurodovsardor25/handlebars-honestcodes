fd8ebe312a5a919fb4072e0a177aa6c67c3bc656
Tojimurodovsardor25 <86824552+Tojimurodovsardor25@users.noreply.github.com>
2022-02-17 18:29:52 +0500

    davomi bor

--- routes/user.js
+++ routes/user.js
@@ -1,63 +1,61 @@
-const { Router } = require('express')
-const router = Router()
-const Users = require('../models/Users')
-const bcrypt = require('bcryptjs')
-const fileUpload = require('../middleware/fileUpload.js')
+const { Router } = require("express");
+const router = Router();
+const Users = require("../models/Users");
+const bcrypt = require("bcryptjs");
+const fileUpload = require("../middleware/fileUpload.js");
 
-router.get('/signin', (req, res, next) => {
-    res.render('user/signin', {
-        title: 'User sign in'
-    })
-})
-
-router.get('/signout', (req, res) => {
-    req.session.destroy(() => {
-        res.redirect('/user/signin')
-    })
-})
-
-router.post('/signin', async (req, res) => {
-    try {
-        const { nickname, password } = req.body
-
-
-        const candidateUser = await Users.findOne({ nickname })
-        if (candidateUser) {
-            const areSame = await bcrypt.compare(password, candidateUser.password) //true || false
-            if (areSame) {
-                req.session.userAuth = true
-                req.session.users = candidateUser
-                res.redirect('/users')
-            } else {
-                res.redirect('/user/signin')
-            }
-        } else {
-            res.redirect('/user/signin')
-        }
-    } catch (error) {
-        console.log(error);
-    }
+router.get("/signin", (req, res, next) => {
+  res.render("user/signin", {
+    title: "User sign in",
+  });
 });
 
-router.get('/signup', (req, res) => {
-    res.render('user/signup', {
-        title: 'User sign up'
-    })
-})
+router.get("/signout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/user/signin");
+  });
+});
 
-router.post('/signup', async (req, res) => {
-    const { nickname, name, password } = req.body
-    const userHasPassword = await bcrypt.hash(password, 10)
+router.post("/signin", async (req, res) => {
+  try {
+    const { nickname, password } = req.body;
 
-    const users = new Users({
-        nickname,
-        name,
-        password: userHasPassword,
-    })
+    const candidateUser = await Users.findOne({ nickname });
+    if (candidateUser) {
+      const areSame = await bcrypt.compare(password, candidateUser.password); //true || false
+      if (areSame) {
+        req.session.userAuth = true;
+        req.session.users = candidateUser;
+        res.redirect("/users");
+      } else {
+        res.redirect("/user/signin");
+      }
+    } else {
+      res.redirect("/user/signin");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-    await users.save()
-    res.redirect('/user/signin')
-})
+router.get("/signup", (req, res) => {
+  res.render("user/signup", {
+    title: "User sign up",
+  });
+});
 
+router.post("/signup", async (req, res) => {
+  const { nickname, name, password } = req.body;
+  const userHasPassword = await bcrypt.hash(password, 10);
 
-module.exports = router
+  const users = new Users({
+    nickname,
+    name,
+    password: userHasPassword,
+  });
+
+  await users.save();
+  res.redirect("/user/signin");
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,17 @@
-const { Router } = require('express')
-const router = Router()
-const user = require('../middleware/user')
-// const Users = require('../models/Users')
+const { Router } = require("express");
+const router = Router();
+const user = require("../middleware/user");
+const Users = require("../models/Users");
 
-router.get('/', user, async (req, res) => {
-  // const users = await Users.find()
+router.get("/", user, async (req, res) => {
+  await Users.find();
   console.log(req.session.users);
-  const userOB = req.session.users
-  res.render('users/index', {
-    title: 'User page index',
-    layout: 'index',
-    userOB
-  })
-})
+  const userOB = req.session.users;
+  res.render("users/index", {
+    title: "User page index",
+    layout: "index",
+    userOB,
+  });
+});
 
-module.exports = router
+module.exports = router;
